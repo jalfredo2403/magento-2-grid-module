e40e35918c134aaedb1df90faec36b574f26e352
Jose hernandez <jahernadezp@gmail.com>
2023-12-29 16:25:18 -0500

    End code of the admin grid

---
+++ Josemage/Josegrid/Controller/Adminhtml/Department/Save.php
@@ -0,0 +1,79 @@
+<?php
+namespace Josemage\Josegrid\Controller\Adminhtml\Department;
+
+use Magento\Backend\App\Action;
+
+class Save extends Action
+{
+    /**
+     * @var \Josemage\Josegrid\Model\Department
+     */
+    protected $_model;
+
+    /**
+     * @param Action\Context $context
+     * @param \Josemage\Josegrid\Model\Department $model
+     */
+    public function __construct(
+        Action\Context $context,
+        \Josemage\Josegrid\Model\Department $model
+    ) {
+        parent::__construct($context);
+        $this->_model = $model;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function _isAllowed()
+    {
+        return $this->_authorization->isAllowed('Josemage_Josegrid::department_save');
+    }
+
+    /**
+     * Save action
+     *
+     * @return \Magento\Framework\Controller\ResultInterface
+     */
+    public function execute()
+    {
+        $data = $this->getRequest()->getPostValue();
+        /** @var \Magento\Backend\Model\View\Result\Redirect $resultRedirect */
+        $resultRedirect = $this->resultRedirectFactory->create();
+        if ($data) {
+            $model = $this->_model;
+
+            $id = $this->getRequest()->getParam('id');
+            if ($id) {
+                $model->load($id);
+            }
+
+            $model->setData($data);
+
+            $this->_eventManager->dispatch(
+                'jobs_department_prepare_save',
+                ['deparment' => $model, 'request' => $this->getRequest()]
+            );
+
+            try {
+                $model->save();
+                $this->messageManager->addSuccess(__('Department saved'));
+                $this->_getSession()->setFormData(false);
+                if ($this->getRequest()->getParam('back')) {
+                    return $resultRedirect->setPath('*/*/edit', ['id' => $model->getId(), '_current' => true]);
+                }
+                return $resultRedirect->setPath('*/*/');
+            } catch (\Magento\Framework\Exception\LocalizedException $e) {
+                $this->messageManager->addError($e->getMessage());
+            } catch (\RuntimeException $e) {
+                $this->messageManager->addError($e->getMessage());
+            } catch (\Exception $e) {
+                $this->messageManager->addException($e, __('Something went wrong while saving the department'));
+            }
+
+            $this->_getSession()->setFormData($data);
+            return $resultRedirect->setPath('*/*/edit', ['entity_id' => $this->getRequest()->getParam('id')]);
+        }
+        return $resultRedirect->setPath('*/*/');
+    }
+}
--- Josemage/Josegrid/Setup/UpgradeSchema.php
+++ Josemage/Josegrid/Setup/UpgradeSchema.php
@@ -134,6 +134,40 @@ class UpgradeSchema implements UpgradeSchemaInterface
             $installer->getConnection()->createTable($table);
         }
 
+        if (version_compare($context->getVersion(), '1.0.0.2') < 0) {
+
+            /**
+             * Add full text index to our table department
+             */
+
+            $tableName = $installer->getTable('josegrid_department');
+            $fullTextIntex = array('name'); // Column with fulltext index, you can put multiple fields
+
+
+            $setup->getConnection()->addIndex(
+                $tableName,
+                $installer->getIdxName($tableName, $fullTextIntex, \Magento\Framework\DB\Adapter\AdapterInterface::INDEX_TYPE_FULLTEXT),
+                $fullTextIntex,
+                \Magento\Framework\DB\Adapter\AdapterInterface::INDEX_TYPE_FULLTEXT
+            );
+
+            /**
+             * Add full text index to our table jobs
+             */
+
+            $tableName = $installer->getTable('josegrid_jobs');
+            $fullTextIntex = array('title', 'type', 'location', 'description'); // Column with fulltext index, you can put multiple fields
+
+
+            $setup->getConnection()->addIndex(
+                $tableName,
+                $installer->getIdxName($tableName, $fullTextIntex, \Magento\Framework\DB\Adapter\AdapterInterface::INDEX_TYPE_FULLTEXT),
+                $fullTextIntex,
+                \Magento\Framework\DB\Adapter\AdapterInterface::INDEX_TYPE_FULLTEXT
+            );
+
+        }
+
         $installer->endSetup();
     }
 }
